3e16d3393458fa43c93b0ec719a571eb14079836
Ashish <ashishbyadav400@gmail.com>
2017-10-31 00:20:24 +0530

    3rd Commit

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php if (have_posts()) : ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Custom Theme</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+   </head>
+    <body>
+
+<!-- <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; ?>
-<?php endif; ?>
+<?php endif; ?> -->
+        
+        
+    </body>
+</html>
