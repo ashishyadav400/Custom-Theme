3d0050bc3e999f79b8b0e4da5629b9e70b4ed1d5
Ashish Yadav <ashishbyadav400@gmail.com>
2017-11-01 23:54:30 +0530

    updated Header and Footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://github.com/ashishyadav400/Custom-Theme
+ *
+ * @package Custom Theme
+ *
+ * @version 1
+ */
+
+?>
---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://github.com/ashishyadav400/Custom-Theme
+ *
+ * @package Custom Theme
+ * 
+ * @version 1
+ */
+
+?>
